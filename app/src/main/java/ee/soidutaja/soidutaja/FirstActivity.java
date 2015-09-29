beb03ff25d31e725ba14439605aa3e09e122d2f0
seep <rehesaare@msn.com>
2015-09-29 12:51:42 +0300

    initial commit

--- app/src/main/java/ee/soidutaja/soidutaja/FirstActivity.java
+++ app/src/main/java/ee/soidutaja/soidutaja/FirstActivity.java
@@ -11,6 +11,7 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+		system.out.println("LAMMAS!");
     }
 
     @Override
