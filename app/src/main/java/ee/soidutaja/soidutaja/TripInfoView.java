2bbe9f750184b9c5e85a22c2f0271c0b45c35d1e
tonis <rehesaare@msn.com>
2015-10-08 15:40:21 +0300

    tere jälle!
    Tegelejad: KP,VL ja TP
    
    Tegime ära dialoogi asja

--- app/src/main/java/ee/soidutaja/soidutaja/TripInfoView.java
+++ app/src/main/java/ee/soidutaja/soidutaja/TripInfoView.java
@@ -1,5 +1,8 @@
 package ee.soidutaja.soidutaja;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +30,21 @@ public class TripInfoView extends AppCompatActivity {
         takeFreeSpaceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new AlertDialog.Builder(TripInfoView.this)
+                        .setTitle("Kohakinnitus")
+                        .setMessage("Kas kinnitan su koha?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue to work with database
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothiogelng
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
 
             }
         });
