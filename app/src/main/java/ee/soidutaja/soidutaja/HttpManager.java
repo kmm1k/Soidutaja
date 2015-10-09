46f4e9fcd56d03b07252d08b86a0aefea31db8bc
Virx <virgo95-@hotmail.com>
2015-10-10 02:35:28 +0300

    klassid, mis jäid eelmisest commitist millegipärast maha

---
+++ app/src/main/java/ee/soidutaja/soidutaja/HttpManager.java
@@ -0,0 +1,63 @@
+package ee.soidutaja.soidutaja;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Virx on 9.10.2015.
+ */
+public class HttpManager {
+    public static String getData(RequestPackage p) {
+        BufferedReader reader = null;
+        String uri = p.getUri();
+
+        if(p.getMethod().equals("GET")) {
+            uri += "?" + p.getEncodedParams();
+        }
+
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod(p.getMethod());
+            Log.d("lammas", "urli sai k2tte");
+
+            if(p.getMethod().equals("POST")) {
+                con.setDoOutput(true);
+                OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+                writer.write(p.getEncodedParams());
+                Log.d("lammas", "hakkas POSTima");
+                writer.flush();
+            }
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            Log.d("lammas", "stringbuilderi tegi ka 2ra");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            Log.d("lammas", sb.toString());
+            return sb.toString();
+        } catch (Exception e) {
+            Log.d("lammas", "http manager on putsis");
+            e.printStackTrace();
+            return null;
+        } finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
