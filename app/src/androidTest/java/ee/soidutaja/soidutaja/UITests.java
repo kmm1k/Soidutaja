289940965eefd61fe8bf1afadd14f0304deda2bb
VIRX <virgo95-@hotmail.com>
2015-10-17 22:03:26 +0300

    Parandab natuke testi

--- app/src/androidTest/java/ee/soidutaja/soidutaja/UITests.java
+++ app/src/androidTest/java/ee/soidutaja/soidutaja/UITests.java
@@ -1,5 +1,6 @@
 package ee.soidutaja.soidutaja;
 
+import android.content.DialogInterface;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -17,10 +18,14 @@ import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withInputType;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -46,5 +51,10 @@ public class UITests {
         onData(allOf(is(instanceOf(String.class)), is("Turi"))).perform(click());
 
         onView(withId(R.id.nextButton)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+
+        onView(withId(R.id.takeSpaceInCar)).perform(click());
+
+        //TODO mõtle välja kuidas dialogi buttoneid klikkida saab
     }
 }
